b43a070f183740a6b43c1f86d8334bd0fe099909
Senad <senadutah@gmail.com>
2020-07-02 20:30:42 -0600

    added uv index and styling

--- script.js
+++ script.js
@@ -13,7 +13,30 @@ button.addEventListener ("click", function(event){
                     console.log (data);
 
                     var temp = document.getElementById ("temperature");
-                    temp.textContent = data.main.temp - 273.15
+                    temp.textContent = (data.main.temp - 273.15)+ " Temperature"
+
+                    var humidity = document.getElementById ("humidity")
+                    humidity.textContent = data.main.humidity + " Humidity"
+
+                    
+                    var windspeed = document.getElementById ("windspeed")
+                    windspeed.textContent = data.wind.speed + " Wind speed"
+                    
+                    
+                    fetch (`http://api.openweathermap.org/data/2.5/uvi/forecast?appid=${API_KEY}&lat=${data.coord.lat}&lon=${data.coord.lon}&cnt=0`).then(
+                        arsp => {
+                            (arsp.json()).then (
+                                data => {
+                                    var uvindex = document.getElementById ("uvindex")
+                                    uvindex.textContent = data[0].value + " UV Index"
+                                    if (data[0].value > 10) uvindex.classList.add("text-danger")
+                                    else if (data[0].value > 5) uvindex.classList.add("text-warning")
+                                    else uvindex.classList.add("text-success")
+                }
+            )
+        }
+    )
+
                 }
             );
         }
